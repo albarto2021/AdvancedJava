6675dd361d554fab03f00e51105d07a619f94b4e
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-08-12 21:29:47 +0300

    gnrc

---
+++ src/InnerOuterClass/InnerOuterClass.java
@@ -0,0 +1,19 @@
+package InnerOuterClass;
+
+// Inner Class yapisi Javada kullanilabilir
+// Eger bir class sadece tek bir class içerisinde kullanilacaksa yani başka bir classtan çağtilmayacaksa,
+// inner class olarak tanımlanabilir.
+// Inner class mantiği classşari gorevlerine gore katogorize etmeyi saglar.
+// Inner class ektra Encapsulation saglamiş olur. Örneğin InnerClass içerisindeki bir değişkene erişmek için
+// sırası ile erişim yapilmasi gerekir. (OuterClass.InnerClass.degisken)
+
+public class InnerOuterClass {
+    // Inner class
+    public class InnerClass1 {
+
+    }
+    // Inner class
+    public class InnerClass2 {
+
+    }
+}
---
+++ src/InnerOuterClass/MultipleClass.java
@@ -0,0 +1,16 @@
+package InnerOuterClass;
+
+public class MultipleClass {
+
+
+
+}
+
+class DigerClass1 {
+
+}
+
+
+class DigerClass2 {
+
+}
