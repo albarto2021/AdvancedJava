8bbefc574e578dda32d2519ce4e15d60d7aa63b5
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-08-13 23:28:58 +0300

    s01

--- src/generics/G07GenericInterface.java
+++ src/generics/G07GenericInterface.java
@@ -2,27 +2,26 @@ package generics;
 
 public class G07GenericInterface {
     public static void main(String[] args) {
-        usAlma<Float> floatUs = new usAlma<>();
+        usAlma<Float> floatUs= new usAlma<>();
         System.out.println(floatUs.usAl(5f, 2f));
 
-        usAlma<Integer> intUs = new usAlma<>();
+        usAlma<Integer> intUs= new usAlma<>();
         System.out.println(intUs.usAl(2, 3));
 
-        usAlma<Double> doubleUs = new usAlma<>();
+        usAlma<Double> doubleUs= new usAlma<>();
         System.out.println(doubleUs.usAl(3.0, 100.0));
-
     }
 }
 
-// Generic interface tanımlaması
+// Generic interface tanımlamasi
 interface Matematik <T extends Number> {
-    double usAl(T t, T u);
+    double usAl(T t ,T u);
 }
 
-// Generic classımızı Matematik Interface'inin implament etti.
+// Generic classımız Matematik interface'inin implement etti
 class usAlma <T extends Number> implements Matematik<T> {
     @Override
-    public double usAl(T t, T u){
+    public double usAl(T t, T u) {
         return Math.pow(t.doubleValue(), u.doubleValue());
     }
 }
