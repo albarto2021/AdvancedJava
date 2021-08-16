d3cf50aef864547d275f5275efd63b9837d72a2b
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-08-17 00:26:08 +0300

    DeadLock

--- src/multiThreading/MT07ThreadPool.java
+++ src/multiThreading/MT07ThreadPool.java
@@ -22,7 +22,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class Mt07ThreadPool {
+public class MT07ThreadPool {
 
     public static void main(String[] args) {
         // Thread havuzunu olustur.
--- src/multiThreading/MT11DeadLockOrnek.java
+++ src/multiThreading/MT11DeadLockOrnek.java
@@ -1,12 +1,17 @@
 package multiThreading;
 
 /*  ===================================================== DEADLOCK======================================================
-
     Deadlock aslında bir kısır döngüdür ve karşılıklı thread'lerin kilitlediği kaynakların salınmasının beklemesi ile
     oluşur.
 
- */
+    DeadLock genelde nested (iç içe) kaynak erişimi sırasında oluşur. Birbirinde habersiz Thread'ler ters sırada
+    kaynaklara nested olarak erişmek isterse DeadLock oluşması muhtemeldir.
 
+    DeadLock'dan kacınmak icin
+    - Nested kaynak erişimi yapılmamalıdır.
+    - Bir kaynağı kilitleyen Thread'in O kaynağı bırakmadan başka bir kaynağa erişimi engellenebilir.
+
+ */
 public class MT11DeadLockOrnek {
 
     public static void main(String[] args) {
